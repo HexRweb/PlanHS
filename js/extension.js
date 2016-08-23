40335a9c2f93018b08b4e06ad8e267b1707401a4
Vikas <vikaspotluri123@gmail.com>
2016-08-23 14:06:46 -0500

    Update Extension Library

--- js/extension.js
+++ js/extension.js
@@ -2,16 +2,24 @@
 * PlanView Chrome Extension Library
 */
 
-window.pv = pv ||
+window.pv = window.pv ||
 {
 	getOption: function(option)
 	{
-
+		return localStorage.getItem(option);
 	},
-	updateOption: function(option,value)
+	updateOption: function(option,value,callback)
 	{
-
-	}
+		var current = localStorage.getItem(option);
+		if(pv.containsDifference(current,value))
+		{
+			localStorage.setItem(option,value);
+			chrome.storage.sync.set({option: value});
+			pv.pushChange("UPDATE","pv.updateOption",current,value,{"format":"STRING","syncUpdated":true});
+			return true;
+		}
+		else return false;
+	},
 	setOption: function(option,value)
 	{
 		return pv.updateOption(option,value);
@@ -19,13 +27,14 @@ window.pv = pv ||
 	pushChange: function(type,initiator,previous,current,otherInfo)
 	{
 		/*TODO:Add code to this. I have no idea how I'm going to implement it but it's supposed to basically serve as an undo*/
+		console.log("Change received:",type,initiator,previous,current,otherInfo);
 		return;
 	},
 	containsDifference: function(before, after)
 	{
 		return !(after === before)
-	}
-	email:
+	},
+	emails:
 	{
 
 		getBlock: function(block)
@@ -39,23 +48,23 @@ window.pv = pv ||
 			{
 				current[block] = email;
 				pv.updateOption("emails",JSON.stringify(current));
-				pv.pushChange("UPDATE","email.updateBlock",previous,email,{"block":block,{"format":"STRING"}});
+				pv.pushChange("UPDATE","email.updateBlock",previous,email,{"block":block,"format":"STRING"});
 				return true;
 			}
 			return false;
 		},
 		setBlock: function(block,email)
 		{
-			return pv.email.updateBlock(block,email);
+			return pv.emails.updateBlock(block,email);
 		},
 		resetEmails: function()
 		{
 			var del = JSON.parse(pv.getOption("emails"));
 			for(toDel in del)
 			{
-				pv.email.updateBlock(toDel,"");
+				pv.emails.updateBlock(toDel,"");
 			}
-			pv.pushChange("RESET","email.resetEmails",del,pv.JSON.parse(getOption("emails")),{"format":"JSON"});
+			pv.pushChange("RESET","email.resetEmails",del,JSON.parse(pv.getOption("emails")),{"format":"JSON"});
 		},
 		workabale: function(check)
 		{
@@ -80,19 +89,42 @@ window.pv = pv ||
 		},
 		updateBlock: function(block,link)
 		{
-
+			var current = JSON.parse(pv.getOption("links")), previous = current[block];
+			if(pv.containsDifference(previous,link))
+			{
+				current[block] = link;
+				pv.updateOption("links",JSON.stringify(current));
+				pv.pushChange("UPDATE","links.updateBlock",previous,link,{"block":block,"format":"STRING"});
+				return true;
+			}
+			return false;
 		},
 		setBlock: function(block,link)
 		{
-			return pv.links.updateBlock(block,email);
+			return pv.links.updateBlock(block,link);
 		},
 		resetLinks: function()
 		{
-
+			var del = JSON.parse(pv.getOption("links"));
+			for(toDel in del)
+			{
+				pv.links.updateBlock(toDel,"");
+			}
+			pv.pushChange("RESET","links.resetLinks",del,JSON.parse(pv.getOption("links")),{"format":"JSON"});
 		},
 		workabale: function(check)
 		{
 			return ((typeof check !== "undefined") && (check !== "") && (check !== null));
+		},
+		create: function()
+		{
+			var old = pv.getOption("links");
+			pv.updateOption("links","{}");
+			for(var i = 1; i <= 8; i++)
+			{
+				pv.links.setBlock(i,""); //Even though set is deprecated it's more definitive of what we're doing
+			}
+			pv.pushChange("REBUILD","links.create",JSON.parse(old),JSON.parse(pv.getOption("emails")),{"format":"JSON"})
 		}
 	}
 };
