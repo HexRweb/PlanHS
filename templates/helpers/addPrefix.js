0d7f781cf8c65ce4d5afd2618c1bf854398c0d94
Vikas <vikaspotluri123@gmail.com>
2016-09-07 16:48:31 -0500

    See description
    - Minor style size updates
    - Add bold class
    - Import Roboto bold font
    - Remove compiled binaries from fonts folder
    - Superscript the stuff after numbers in notes page
    - Stronger bold in notes page

--- templates/helpers/addPrefix.js
+++ templates/helpers/addPrefix.js
@@ -1,11 +1,15 @@
-module.exports.addPrefix = function(number)
+module.exports.addPrefix = function(number,sup)
 {
+	sup = sup || true;
+	var prefix = "", suffix ="";
+	if(sup)
+		prefix="<sup>",suffix="</sup>";
 	if(number > 3)
-		return number + "th";
+		return number + prefix + "th" + suffix;
 	switch(number)
 	{
-		case 1: return "1st";
-		case 2: return "2nd";
-		case 3: return "3rd";
+		case 1: return number + prefix + "st" + suffix;
+		case 2: return number + prefix + "nd" + suffix;
+		case 3: return number + prefix + "rd" + suffix;
 	}
 };
