60f7ad21c52df59017b3dc482a726bab2ffa05d8
Vikas <potlurivikas@gmail.com>
2016-09-27 11:28:47 -0500

    Tentative structure for new framework

---
+++ src/assets/js/new.js
@@ -0,0 +1,77 @@
+window.pv = window.pv ||
+{
+    getOption: function(){},
+    updateOption:function(){},
+    util:
+    {
+        containsDifference: function(){}
+    },
+    init
+    {
+        settings: function()
+        {
+
+        },
+        global: function()
+        {
+
+        },
+        notes: function()
+        {
+
+        }
+    },
+    block:
+    {
+        getBlock:function(){},
+        addBlock:function(){}
+        attributes:
+        {
+            notes:
+            {
+                get:function(){},
+                update:function(){}
+            },
+            email:
+            {
+                get:function(){},
+                update:function(){}
+            },
+            name:
+            {
+                ge:function(){},
+                update:function(){}
+            },
+            links:
+            {
+                get:function(){},
+                update:function(){}
+            }
+        }
+    },
+    theme:
+    {
+        primary: "",
+        secondary: "",
+        nav:
+        {
+            side:
+            {
+                background:"",
+                itemBackground:"",
+                itemColor: ""
+            },
+            main
+            {
+                background:"",
+                color:""
+            }
+        }
+    },
+    calendar:
+    {
+        get:function(){},
+        update:function(){},
+        getFrame:function(){}
+    }
+}
